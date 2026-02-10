768fed0c889e4a1c5c6705c752c41b52c29f72a0
Xeks4237 <amid.muradov111@gmail.com>
2026-02-10 16:35:45 +0400

    Moved configuration variables in to setup function

--- zshgod.plugin.zsh
+++ zshgod.plugin.zsh
@@ -29,67 +29,20 @@
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
-# Set variable to absolute path to the directory containing this file
-# ${0} Gives path to this file
-# :A in ${0} makes it to be absolute without path through symlinks
-ZSHGOD_HOME="${0:A:h}"
-
 # Add directory with prompt files to fpath
-fpath+=( $ZSHGOD_HOME/functions )
-
-# [ Configuration Variables ]
-# Variable which sets amount of exectime after exectime is not hided
-export ZSHGOD_EXECTIME_MIN=5
-
-# Builtin variable which sets indentation for prompts right side
-export ZLE_RPROMPT_INDENT=0
-
-# Disable multilined prompt by default
-export ZSHGOD_MULTILINED=false
-
-# INFO: I used Catppuccin Mocha Colors from: https://github.com/catppuccin
-# Main colors
-export ZSHGOD_THM_CATPPUCCIN_ROSEWATER='#F5E0DC'
-export ZSHGOD_THM_CATPPUCCIN_FLAMINGO='#F2CDCD'
-export ZSHGOD_THM_CATPPUCCIN_PINK='#F5C2E7'
-export ZSHGOD_THM_CATPPUCCIN_MAUVE='#CBA6F7'
-export ZSHGOD_THM_CATPPUCCIN_RED='#F38BA8'
-export ZSHGOD_THM_CATPPUCCIN_MAROON='#EBA0AC'
-export ZSHGOD_THM_CATPPUCCIN_PEACH='#FAB387'
-export ZSHGOD_THM_CATPPUCCIN_YELLOW='#F9E2AF'
-export ZSHGOD_THM_CATPPUCCIN_GREEN='#A6E3A1'
-export ZSHGOD_THM_CATPPUCCIN_TEAL='#94E2D5'
-export ZSHGOD_THM_CATPPUCCIN_SKY='#89DCEB'
-export ZSHGOD_THM_CATPPUCCIN_SAPPHIRE='#74C7EC'
-export ZSHGOD_THM_CATPPUCCIN_BLUE='#89B4FA'
-export ZSHGOD_THM_CATPPUCCIN_LAVENDER='#B4BEFE'
-
-# Main surface and overlay colors
-export ZSHGOD_THM_CATPPUCCIN_SUBTEXT_1='#A6ADC8'
-export ZSHGOD_THM_CATPPUCCIN_SUBTEXT_0='#BAC2DE'
-export ZSHGOD_THM_CATPPUCCIN_FOREGROUND='#CDD6F4'
-export ZSHGOD_THM_CATPPUCCIN_OVERLAY_2='#9399B2'
-export ZSHGOD_THM_CATPPUCCIN_OVERLAY_1='#7F849C'
-export ZSHGOD_THM_CATPPUCCIN_OVERLAY_0='#6C7086'
-export ZSHGOD_THM_CATPPUCCIN_SURFACE_2='#585B70'
-export ZSHGOD_THM_CATPPUCCIN_SURFACE_1='#45475A'
-export ZSHGOD_THM_CATPPUCCIN_SURFACE_0='#313244'
-export ZSHGOD_THM_CATPPUCCIN_MANTLE='#181825'
-export ZSHGOD_THM_CATPPUCCIN_CRUST='#11111B'
-export ZSHGOD_THM_CATPPUCCIN_BACKGROUND='#1E1E2E'
+fpath+=( ${0:A:h}/functions )
 
 # zstyle options to enable or disable some vcs systems which you don't use
 # zstyle ':vcs_info:*' disable git svn
 zstyle ':vcs_info:*' enable bzr cdv cvs darcs fossil git hg mtn p4 svk svn tla
 
 # Autoload prompt files with its lib files to make usage of builtin zsh lazy loading for functions
-# Every file contains one function with same name as file name
 # NOTE: always use '-Uz' to supress alias expansion and to use zsh styly autoloading
-# [ Autoload setup function ]
-autoload -Uz prompt_zshgod_setup
+# Every file contains one function with same name as file name
 
-# [ Autoload functions with no styling ] (backend only or raw text)
+# [ Autoload technical functions ]
 autoload -Uz \
+	prompt_zshgod_setup \
 	prompt_zshgod_preexec \
 	prompt_zshgod_precmd \
 	prompt_zshgod_exectime
